48f3671fa76b9f8d8293b5a345f7e8026c47abaa
christopherflores9312 <christopherflores9312@gmail.com>
2023-04-24 14:54:35 -0700

    added comments

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-// Import the required packages
+// Import the inquirer package for prompting user input
 const inquirer = require('inquirer');
+// Import the fs (file system) module for writing to files
 const fs = require('fs');
+// Import the generateMarkdown function from the generateMarkdown module
 const generateMarkdown = require('./utils/generateMarkdown');
-
+// Define an array of questions to be prompted to the user
 const questions = [
     {
       type: 'input',
@@ -52,23 +54,25 @@ const questions = [
     }
   ];
 
-// Create a function to initialize the app
+// Define the init function to initialize the app
 function init() {
-  // Prompt the user for input
+  // Use inquirer to prompt the user for input based on the defined questions
   inquirer.prompt(questions).then((answers) => {
     // Generate the content of the README.md file using the generateMarkdown function
     const readmeContent = generateMarkdown(answers);
 
-    // Write the generated content to a new README.md file
+    // Write the generated content to a new README.md file in the assets directory
     fs.writeFile('./assets/README.md', readmeContent, (err) => {
       if (err) {
+        // Log any errors that occurred during file writing
         console.log(err);
       } else {
+        // Log a success message indicating that the README.md file was created
         console.log('Successfully created README.md');
       }
     });
   });
 }
 
-// Function call to initialize the app
+// Call the init function to initialize and run the app
 init();
